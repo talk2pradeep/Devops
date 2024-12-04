8ecc1b5f578caee128eb3077eab585fcd400a7c2
Pradeep Kumar <pradeepkumar@Pradeeps-MacBook-Air.local>
2024-12-04 22:42:05 +0530

    my changes

--- src/main/java/com/devopsdemo/springboot_app/SpringbootAppApplication.java
+++ src/main/java/com/devopsdemo/springboot_app/SpringbootAppApplication.java
@@ -2,10 +2,17 @@ package com.devopsdemo.springboot_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootAppApplication {
 
+	@GetMapping("/start")
+	public  String start(){
+		return "welcome to spring boot";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAppApplication.class, args);
 	}
